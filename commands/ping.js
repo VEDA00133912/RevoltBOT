05e440cef8ee36656bf0bb70c221f2e0dcd2057b
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-01-20 07:25:48 +0000

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,21 @@
+const axios = require('axios');
+
 exports.default = {
     name: 'ping',
-    code(message) {
+    async code(message) {
         try {
-            message.reply('🏓Pong!');
+            const start = Date.now();
+            await axios.get('https://api.revolt.chat');
+            const embed = {
+                description: `
+                **Bot Latency**: ${Date.now() - message.createdAt}ms
+                **API Latency**: ${Date.now() - start} ms`
+            };
+
+            await message.reply({ embeds: [embed] });
         } catch (error) {
             console.error(error);
-            message.reply('コマンドの実行中にエラーが発生しました');
+            await message.reply('コマンドの実行中にエラーが発生しました');
         }
     },
 };
