d8a0f9c12edb6975f6fd6b68a7120ae5512da5ef
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-01-20 06:31:03 +0000

    pingにエラーハンドリング追加

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,11 @@
 exports.default = {
     name: 'ping',
     code(message) {
-        message.reply('🏓Pong!');
+        try {
+            message.reply('🏓Pong!');
+        } catch (error) {
+            console.error(error);
+            message.reply('コマンドの実行中にエラーが発生しました');
+        }
     },
 };
